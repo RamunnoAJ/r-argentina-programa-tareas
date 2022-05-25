33411c69addf846b106afc3810a954ca8d17cde2
RamunnoAJ <ramunnoagustin@gmail.com>
2022-05-25 14:45:37 -0300

    Crea archivos de tarea 4
    Aquí cree los archivos pertinentes a html, css y js para realizar la última tarea de la clase 5

---
+++ clase-5/tarea4.js
@@ -0,0 +1,14 @@
+//TAREA: En otro archivo distinto,
+// Crear una lista de <ol> y <li> que contengan sólo números.
+// Convertir esos números a un array y:
+// 1. calcular el promedio y mostrarlo en un <em> pre-creado con el texto "El promedio es..."
+// 2. obtener el número más pequeño y mostrarlo en un <em> pre-creado con el texto "El número más pequeño es..."
+// 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
+// 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
+
+const numerosLista = document.querySelectorAll(".numero-lista");
+console.log(numerosLista);
+
+function numeroPromedio(numerosLista) {
+    for (let i = 0;i< numerosLista.length; i++)
+}
