f82d9bd9fd816ac0b5d8e5425151241fd0cf96b4
RamunnoAJ <ramunnoagustin@gmail.com>
2022-05-30 16:40:23 -0300

    Clase 6 tarea 1 completa
    En este commit realicé la primera tarea de la clase 6

--- clase-6/tarea-clase-6.js
+++ clase-6/tarea-clase-6.js
@@ -6,6 +6,110 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
 */
 
+document.querySelector(".siguiente").onclick = function () {
+  const $miembrosFamilia = document.querySelector(".miembros-familia");
+  const miembrosFamlia = Number($miembrosFamilia.value);
+
+  borrarMiembrosAnteriores();
+  crearMiembros(miembrosFamlia);
+
+  return false;
+};
+
+function crearMiembros(miembrosFamilia) {
+  for (let i = 0; i < miembrosFamilia; i++) {
+    crearMiembro(i);
+  }
+}
+
+function crearMiembro(indice) {
+  const $div = document.createElement("div");
+  $div.className = "miembro";
+
+  const $label = document.createElement("label");
+  $label.textContent = `Edad del miembro ${indice + 1}`;
+  const $input = document.createElement("input");
+  $input.type = "number";
+
+  $div.appendChild($label);
+  $div.appendChild($input);
+
+  const $miembros = document.querySelector(".miembros");
+  $miembros.appendChild($div);
+}
+
+function borrarMiembrosAnteriores() {
+  const $miembros = document.querySelectorAll(".miembro");
+  for (let i = 0; i < $miembros.length; i++) {
+    $miembros[i].remove();
+  }
+}
+
+document.querySelector(".limpiar").onclick = function () {
+  borrarMiembrosAnteriores();
+};
+
+function obtenerEdadMiembros() {
+  const $miembros = document.querySelectorAll(".miembro input");
+  const edades = [];
+
+  for (let i = 0; i < $miembros.length; i++) {
+    edades.push(Number($miembros[i].value));
+  }
+
+  return edades;
+}
+
+document.querySelector(".calcular").onclick = function () {
+  const edades = obtenerEdadMiembros();
+
+  calcularMayorEdad(edades);
+
+  const $mayorEdad = document.querySelector("#mayor-edad");
+  $mayorEdad.textContent = `${calcularMayorEdad(edades)}`;
+
+  const $menorEdad = document.querySelector("#menor-edad");
+  $menorEdad.textContent = `${calcularMenorEdad(edades)}`;
+
+  const $promedioEdad = document.querySelector("#edad-promedio");
+  $promedioEdad.textContent = `${calcularPromedioEdad(edades)}`;
+};
+
+function calcularMayorEdad(array) {
+  let mayorEdad = 0;
+  for (let i = 0; i < array.length; i++) {
+    let edadMiembro = Number(array[i]);
+
+    if (edadMiembro > mayorEdad) {
+      mayorEdad = edadMiembro;
+    }
+  }
+
+  return mayorEdad;
+}
+
+function calcularMenorEdad(array) {
+  let menorEdad = array[0];
+  for (let i = 0; i < array.length; i++) {
+    let edadMiembro = Number(array[i]);
+
+    if (edadMiembro < menorEdad) {
+      menorEdad = edadMiembro;
+    }
+  }
+
+  return menorEdad;
+}
+
+function calcularPromedioEdad(array) {
+  let promedioEdad = 0;
+  for (let i = 0; i < array.length; i++) {
+    promedioEdad += Number(array[i]);
+  }
+
+  return promedioEdad / array.length;
+}
+
 /*
 TAREA:
 Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
