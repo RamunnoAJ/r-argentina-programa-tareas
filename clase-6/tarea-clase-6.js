39018f21b9b3a67eafa6aeec51cd31000298eedf
RamunnoAJ <ramunnoagustin@gmail.com>
2023-03-02 18:05:28 -0300

    agrega las funciones al archivo js

--- clase-6/tarea-clase-6.js
+++ clase-6/tarea-clase-6.js
@@ -6,12 +6,6 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
 */
 
-import {
-  calcularMayorNumero,
-  calcularMenorNumero,
-  calcularPromedioNumero
-} from './funciones'
-
 const $botonSiguiente = document.querySelector('#boton-siguiente')
 $botonSiguiente.addEventListener('click', () => {
   const $miembrosFamilia = document.querySelector('.miembros-familia')
@@ -163,4 +157,39 @@ function manejarErrores(errores) {
   } else {
     document.querySelector('.miembros-familia').className = ''
   }
+}
+
+function calcularMayorNumero(array) {
+  let mayorNumero = 0
+  for (let i = 0; i < array.length; i++) {
+    let numeroMiembro = Number(array[i])
+
+    if (numeroMiembro > mayorNumero) {
+      mayorNumero = numeroMiembro
+    }
+  }
+
+  return mayorNumero
+}
+
+function calcularMenorNumero(array) {
+  let menorNumero = array[0]
+  for (let i = 0; i < array.length; i++) {
+    let numeroMiembro = Number(array[i])
+
+    if (numeroMiembro < menorNumero) {
+      menorNumero = numeroMiembro
+    }
+  }
+
+  return menorNumero
+}
+
+function calcularPromedioNumero(array) {
+  let promedioNumero = 0
+  for (let i = 0; i < array.length; i++) {
+    promedioNumero += Number(array[i])
+  }
+
+  return promedioNumero / array.length
 }
